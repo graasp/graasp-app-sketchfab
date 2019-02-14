30de5b7ba9f7d363965e430ed027d9bfb0fe8b63
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2019-02-14 10:41:40 +0100

    feat: show qr code to link from app to sketchfab viewer app
    For the selected models, the student will be able to toggle between
    viewing the model and showing a QR code that links to the model on
    the Sketchfab app if the user has it installed on his/her device.
    
    closes #4

---
+++ src/components/common/ModelQrCode.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { QRCode as QrCode } from 'react-qr-svg';
+
+const styles = theme => ({
+  modelQrCode: {
+    padding: theme.spacing.unit * 5,
+  },
+});
+
+const ModelQrCode = ({ uid, classes }) => (
+  <div className={classes.modelQrCode}>
+    <QrCode
+      bgColor="#FFFFFF"
+      fgColor="#000000"
+      level="Q"
+      style={{ width: 256 }}
+      value={`https://sketchfab.com/models/${uid}/embed?api_id=1_api-frame&api_version=1.4.2`}
+    />
+  </div>
+);
+
+ModelQrCode.propTypes = {
+  uid: PropTypes.string.isRequired,
+  classes: PropTypes.shape({}).isRequired,
+};
+
+export default withStyles(styles)(ModelQrCode);
--- src/components/modes/student/StudentView.js
+++ src/components/modes/student/StudentView.js
@@ -1,30 +1,66 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Viewer from '../../common/Viewer';
 import Loader from '../../common/Loader';
+import ModelQrCode from '../../common/ModelQrCode';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
 
 class StudentView extends Component {
   static propTypes = {
     model: PropTypes.string,
+    classes: PropTypes.shape({}).isRequired,
   };
 
   static defaultProps = {
     model: null,
   };
 
+  state = {
+    value: 0,
+  };
+
   componentDidMount() {
     if (window.frameElement) {
-      window.frameElement.style.height = '600px';
+      // 600 is the height of the viewer and 48 the height of the header
+      window.frameElement.style.height = '648px';
     }
   }
 
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
-    const { model } = this.props;
+    const { classes, model } = this.props;
+    const { value } = this.state;
+
     if (!model) {
       return <Loader />;
     }
-    return <Viewer uid={model} />;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="secondary">
+          <Tabs value={value} onChange={this.handleChange}>
+            <Tab label="Model" />
+            <Tab label="QR Code" />
+          </Tabs>
+        </AppBar>
+        {value === 0 && <Viewer uid={model} autoStart={false} />}
+        {value === 1 && <ModelQrCode uid={model} />}
+      </div>
+    );
   }
 }
 
-export default StudentView;
+export default withStyles(styles)(StudentView);
