bc909e87375943a13cc19c3ba4abb5065005b948
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2019-05-02 16:56:59 +0200

    fix: do not register service worker
    closes #18

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import Root from './components/Root';
-import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
@@ -30,5 +29,3 @@ if (module.hot) {
     renderApp(NextRoot, store, history);
   });
 }
-
-registerServiceWorker();
