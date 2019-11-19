12e7dfb105b7b590486ea36ffc8f191abd2f0626
Juan Carlos Farah <farah.juancarlos@gmail.com>
2019-11-19 12:21:07 +0100

    feat: add google analytics tracker

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGa from 'react-ga';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { I18nextProvider } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
@@ -7,6 +8,19 @@ import grey from '@material-ui/core/colors/grey';
 import 'react-toastify/dist/ReactToastify.css';
 import i18nConfig from '../config/i18n';
 import App from './App';
+import {
+  REACT_APP_GRAASP_APP_ID,
+  REACT_APP_GRAASP_DEVELOPER_ID,
+  REACT_APP_VERSION,
+  REACT_APP_GOOGLE_ANALYTICS_ID,
+} from '../config/env';
+
+ReactGa.initialize(REACT_APP_GOOGLE_ANALYTICS_ID);
+ReactGa.ga(
+  'send',
+  'pageview',
+  `/${REACT_APP_GRAASP_DEVELOPER_ID}/${REACT_APP_GRAASP_APP_ID}/${REACT_APP_VERSION}/`
+);
 
 const theme = createMuiTheme({
   palette: {
